50b1c28643592fa3c97c980a8a10ffaf54a1f23e
Pedro <pedrosilonliveira05@gmail.com>
2021-06-01 18:05:37 -0300

    enter

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { render } from 'react-native/Libraries/Renderer/implementations/ReactNativeRenderer-dev';
 import Button from './src/componets/Button'
 import Display from './src/componets/Display'
 
@@ -63,31 +64,32 @@ export default function App() {
     }
   }
 
-
-  return (
-    <View style={styles.container}>
-    <Display value={this.state.displayValue} />
-    <View style={styles.button}>
-      <Button label='AC' triple onClick={this.clearMemory} />
-      <Button label='/' operation onClick={this.setOperation} />
-      <Button label='7' onClick={this.addDigit} />
-      <Button label='8' onClick={this.addDigit} />
-      <Button label='9' onClick={this.addDigit} />
-      <Button label='*' operation onClick={this.setOperation} />
-      <Button label='4' onClick={this.addDigit} />
-      <Button label='5' onClick={this.addDigit} />
-      <Button label='6' onClick={this.addDigit} />
-      <Button label='-' operation onClick={this.setOperation} />
-      <Button label='1' onClick={this.addDigit} />
-      <Button label='2' onClick={this.addDigit} />
-      <Button label='3' onClick={this.addDigit} />
-      <Button label='+' operation onClick={this.setOperation} />
-      <Button label='0' double onClick={this.addDigit} />
-      <Button label='.' onClick={this.addDigit} />
-      <Button label='=' operation onClick={this.setOperation} />
+  render() {
+    return (
+      <View style={styles.container}>
+      <Display value={this.state.displayValue} />
+      <View style={styles.button}>
+        <Button label='AC' triple onClick={this.clearMemory} />
+        <Button label='/' operation onClick={this.setOperation} />
+        <Button label='7' onClick={this.addDigit} />
+        <Button label='8' onClick={this.addDigit} />
+        <Button label='9' onClick={this.addDigit} />
+        <Button label='*' operation onClick={this.setOperation} />
+        <Button label='4' onClick={this.addDigit} />
+        <Button label='5' onClick={this.addDigit} />
+        <Button label='6' onClick={this.addDigit} />
+        <Button label='-' operation onClick={this.setOperation} />
+        <Button label='1' onClick={this.addDigit} />
+        <Button label='2' onClick={this.addDigit} />
+        <Button label='3' onClick={this.addDigit} />
+        <Button label='+' operation onClick={this.setOperation} />
+        <Button label='0' double onClick={this.addDigit} />
+        <Button label='.' onClick={this.addDigit} />
+        <Button label='=' operation onClick={this.setOperation} />
+      </View>
     </View>
-  </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
