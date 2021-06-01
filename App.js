457233bb0f957f18ca95fa8909243f75d9f9f6b4
Pedro <pedrosilonliveira05@gmail.com>
2021-06-01 18:20:33 -0300

    enter

--- App.js
+++ App.js
@@ -85,6 +85,7 @@ export default function App() {
       <Button label='0' double onClick={this.addDigit} />
       <Button label='.' onClick={this.addDigit} />
       <Button label='=' operation onClick={this.setOperation} />
+      <StatusBar style="light" />
     </View>
   </View>
   );
